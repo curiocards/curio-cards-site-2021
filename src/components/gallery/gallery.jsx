7e11073cdb3354bb042faf79313e45c8f0ff245b
Kian <fafrd@users.noreply.github.com>
2021-09-08 10:59:55 -0700

    Handle address lookup failure

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -27,6 +27,7 @@ class Gallery extends React.Component {
       selectedAddress: props.selectedAddress,
       provider: provider,
       loading: true,
+      lookupFailure: false
     };
 
     this.handleAddressSubmit = this.handleAddressSubmit.bind(this);
@@ -82,7 +83,10 @@ class Gallery extends React.Component {
   }
 
   async updateCards(inputAddress) {
-    this.setState({loading: true});
+    this.setState({
+      loading: true,
+      lookupFailure: false
+    });
 
     let cards = _.cloneDeep(this.props.cards);
     let holdings = {};
@@ -94,27 +98,43 @@ class Gallery extends React.Component {
         erc20Promises.push(this.getErc20Balance(inputAddress, i));
       const erc1155Promise = this.getErc1155BalanceBatch(inputAddress);
 
-      const erc20Balances = await Promise.all(erc20Promises);
-      const erc1155Balances = await erc1155Promise;
 
-      for (let i = 0; i < 30; i++) {
-        holdings[i+1] = {
-          "erc1155": parseInt(erc1155Balances[i]._hex, 16),
-          "erc20": parseInt(erc20Balances[i], 10)
-        };
-      }
-      console.debug(`Curio balance for address ${inputAddress}:\n${JSON.stringify(holdings, null, 2)}`);
-    }
+      let fetchSuccess = true;
+      const erc20Balances = await Promise.all(erc20Promises).catch(error => {
+        console.error("Error fetching ERC20 balance;", error);
+        fetchSuccess = false;
+      });
+      const erc1155Balances = await erc1155Promise.catch(error => {
+        console.error("Error fetching ERC1155 balance;", error);
+        fetchSuccess = false;
+      });
 
-    // If an address was provided, set the holdings to this address's supply
-    if (inputAddress && Object.keys(holdings).length > 0) {
-      for (let i = 0; i < cards.length; i++) {
-        if (cards[i].number in holdings) {
-          cards[i].supply = holdings[cards[i].number].erc1155 + holdings[cards[i].number].erc20;
-          cards[i].holdings = holdings[cards[i].number];
-        } else {
-          cards[i].supply = 0;
+      if (fetchSuccess) {
+        for (let i = 0; i < 30; i++) {
+          holdings[i+1] = {
+            "erc1155": parseInt(erc1155Balances[i]._hex, 16),
+            "erc20": parseInt(erc20Balances[i], 10)
+          };
         }
+
+        // Populate address supply
+        if (Object.keys(holdings).length > 0) {
+          for (let i = 0; i < cards.length; i++) {
+            if (cards[i].number in holdings) {
+              cards[i].supply = holdings[cards[i].number].erc1155 + holdings[cards[i].number].erc20;
+              cards[i].holdings = holdings[cards[i].number];
+            } else {
+              cards[i].supply = 0;
+            }
+          }
+        }
+
+        console.debug(`Curio balance for address ${inputAddress}:\n${JSON.stringify(holdings, null, 2)}`);
+      } else {
+        console.log("todo, handle fetch failure");
+        this.setState({
+          lookupFailure: true
+        });
       }
     }
 
@@ -151,8 +171,10 @@ class Gallery extends React.Component {
     }
 
     let gallery = null;
-    if (this.state.loading) {
-      gallery = <p class="warning-message">Loading...</p>;
+    if (this.state.lookupFailure) {
+      gallery = <p className="warning-message">{`Error fetching cards for address ${this.state.selectedAddress}.`}</p>;
+    } else if (this.state.loading) {
+      gallery = <p className="warning-message">Loading...</p>;
     } else if (cards.find(card => card.supply > 0)) {
       gallery = cards.map(card => {
         if (card.holdings != null) {
@@ -189,7 +211,7 @@ class Gallery extends React.Component {
         }
       });
     } else {
-      gallery = <p class="warning-message">{`No cards for address ${this.state.selectedAddress}.`}</p>;
+      gallery = <p className="warning-message">{`No cards for address ${this.state.selectedAddress}.`}</p>;
     }
 
     let leaderboardMessage = null;
@@ -198,6 +220,11 @@ class Gallery extends React.Component {
         <p className="orangeLight center cell border">Curious who has most cards?&#160;<a target="_blank" rel="noopener noreferrer" className="orangeLight underline center cell" href="https://leaderboard.curio.cards/"> See the Leaderboard</a></p>
         
       </div>;
+    }
+
+    let formattedAddress = this.state.selectedAddress;
+    if (formattedAddress && formattedAddress.match(/^0x[a-fA-F0-9]{40}$/)) {
+      formattedAddress = formattedAddress.substring(0, 6) + "..." + formattedAddress.slice(-4);
     }
 
     return (
@@ -212,7 +239,7 @@ class Gallery extends React.Component {
           <div className="nav-inner">
             <form onSubmit={this.handleAddressSubmit} autoComplete="off">
               <input type="text" className="cell border white input__eth" id="address-input" autoComplete="off" spellCheck="false"
-                placeholder="Wallet Lookup" defaultValue={this.state.selectedAddress} disabled={!!this.state.selectedAddress} />
+                placeholder="Address or ENS Lookup" defaultValue={formattedAddress} disabled={!!this.state.selectedAddress} />
               {
                 !!this.state.selectedAddress ?
                   <button className="icon dropdown__icon cell orange"> <img src={close} alt="" /> </button>
