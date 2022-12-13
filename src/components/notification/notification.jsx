374e2003c941477e8f7123acf6a4ce8a639f64d6
Kian <fafrd@users.noreply.github.com>
2022-12-13 12:31:27 -0800

    update snowglobe notification

--- src/components/notification/notification.jsx
+++ src/components/notification/notification.jsx
@@ -8,7 +8,7 @@ class Notification extends React.Component {
   render() {
     return (
         <p className="pink center">
-            ✨ Stay tuned for a holiday announcement this Tuesday, December 13th... ✨
+            ✨ Check out Curio&#39;s Snow Globes! Free mint for Curio Card holders: <a style={{ marginLeft: "0.5em", marginRight: "0.5em", textDecoration: "underline" }} href="https://dao.curio.cards/snowglobes">dao.curio.cards/snowglobes</a> ✨
         </p>
     );
   }
